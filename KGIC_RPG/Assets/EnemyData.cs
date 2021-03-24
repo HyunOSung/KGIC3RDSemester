1669bbddafb21aef2a8afea872bec8112caa028a
SungJunYeop <sierrahotel555@gmail.com>
2021-03-24 09:51:47 +0900

    깃 이그노어 테스트 01

---
+++ KGIC_RPG/Assets/EnemyData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyData : ScriptableObject
+{
+    public List<Attribute> list = new List<Attribute>();
+
+    [System.Serializable]
+    public class Attribute
+    {
+        public int level;
+        public int maxHP;
+        public int baseAttack;
+        public int baseDef;
+        public int moveSpeed;
+        public int turnSpeed;
+        public float attackRange;
+
+    }
+}
